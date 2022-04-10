987df25f08db6987c31edc52fea3fac9898adc3b
domuz86 <59613646+domuz86@users.noreply.github.com>
2022-04-10 21:23:42 +0200

    Update index.js

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,1 +1,17 @@
+// Only change code below this line
+function myMutation(arr) {
+  var word1 = arr[0].toLowerCase().split("");
+  var word2 = arr[1].toLowerCase().split("");
 
+  for (var i = 0; i < word2.length; i++) {
+    if (!word1.includes(word2[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(myMutation(["hello", "hey"]));
+// Only change code above this line
+module.exports = myMutation;
