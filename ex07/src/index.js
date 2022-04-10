34ac103a124185420d6f238dde22b91eb9af662d
domuz86 <59613646+domuz86@users.noreply.github.com>
2022-04-10 21:39:14 +0200

    Update index.js

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -2,23 +2,21 @@
 function multiplyArrayFunction(myArry) {
   var product = 1;
   var sum = 0;
+  var retutnArray = [];
   for (let i = 0; i < myArry.length; i++) {
-      for (let j = 0; j < myArry[i].length; j++) {
-    
-        product *= myArry[i][j];
-        sum += myArry[i][j];
+    for (let j = 0; j < myArry[i].length; j++) {
 
-  
-   }
+      product = product * myArry[i][j];
+      sum = sum + myArry[i][j];
 
- 
-    
+    }
+
   }
- return {
-   product,
-  sum
-}
+
+  retutnArray.push(product);
+  retutnArray.push(sum);
+  return retutnArray;
 }
 // Only change code above this line
-console.log(multiplyArrayFunction([[2],[5,6,7],[8,9]]));
+console.log(multiplyArrayFunction([[2], [5, 6, 7], [8, 9]]));
 module.exports = multiplyArrayFunction;
