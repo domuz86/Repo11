efcdf68e8d5863fde574a2c7de3aadc23f76f1bd
domuz86 <59613646+domuz86@users.noreply.github.com>
2022-04-10 21:11:20 +0200

    Update index.js

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,18 +1,13 @@
-//Create an array here
-
+// Create an array here
 var myArr = [2, 4, 0, 8, 10];
+// End of creating an array
+// Create a function below this line
+function myArrayFunction(myItems) {
+  myItems = myArr;
+  myArr[2] = 6;
 
-//End of creating an array
-//Create a function below this line
-
-function myArrayFunction(myItems){
-
-myArr[2] = myItems;
-myItems = myArr;
-return myItems;
-};
-
-myArrayFunction(6);
-
-//End of creating function
+  return myItems;
+}
+// End of creating a function
+console.log(myArrayFunction(myArr));
 module.exports = myArrayFunction;
